6734d22d2a93ec791ce164970806ec9e608246e6
Prakriti Adhikari <prakritiadhikari2121@gmail.com>
2024-07-05 12:46:36 +0545

    Initialize Firebase and set up React root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import App from './App';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1VpENaB16dCqhaMYsdDDpkNab0o6MGQI",
+  authDomain: "pokemon-681c8.firebaseapp.com",
+  projectId: "pokemon-681c8",
+  storageBucket: "pokemon-681c8.appspot.com",
+  messagingSenderId: "270177776698",
+  appId: "1:270177776698:web:09f05f57aafcdb31833103",
+  measurementId: "G-KR3WSCJ0Y3"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,4 +22,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
